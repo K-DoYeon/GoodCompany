5ca612fe3294a453f0f6be9c30c95a0520d03785
K-DoYeon <124326065+K-DoYeon@users.noreply.github.com>
2023-07-02 21:10:53 +0900

    good company

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function(){
+    // $('.gnb').hover(function(){
+    //     $('.nav-bar').stop().slideDown();
+    // })
+    // $('.nav-bar').mouseleave(function(){
+    //     $('.nav-bar').stop().slideUp();
+    // })
+
+    $('.gnb>li').hover(function(){
+    $('.lnb-s').stop().slideToggle();
+    })
+    
+    
+    
+})
